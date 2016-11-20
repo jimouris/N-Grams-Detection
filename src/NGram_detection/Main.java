07697522a051ef4c9c01d266b98bbd88c97cddce
Nikolas Bompetsis <nbompetsis@di.uoa.gr>
2016-11-20 21:30:57 +0200

    Occurrency is finished

--- src/NGram_detection/Main.java
+++ src/NGram_detection/Main.java
@@ -1,53 +1,85 @@
 package NGram_detection;
 
+import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.HashMap;
+import java.util.Iterator;
 import java.util.Map;
-import java.util.stream.Stream;
 
 /**
  * Created by nikolas on 20/11/16.
  */
 public class Main {
 
-    public static String input_file;
+    public static String input_file = "input.dat";
     public static String output_file = "ngram_list.dat";
 
     public static void main(String[] args) {
 
-        input_file = args[0];
+
         // access arguments
-//        for (int i = 0; i != args.length; ++i) {
-//            String arg = args[i];
-//            if (arg == "-i") {
-//                if (arg != null) input_file = args[i+1];
-//            } else if (arg == "-o") {
-//                if (arg != null) output_file = args[i+1];
-//            } else if (arg == "-h") {
-//                System.out.println("Usage: " + args[0] + " -i inputFile -o outputFile");
-//            } else {
-//                System.out.println("Usage: " + args[0] + " -i inputFile -o outputFile");
-//                System.exit(-1);
-//            }
-//        }
+        for (int i = 0; i != args.length; i++) {
+            if (args[i].equalsIgnoreCase("-i")) {
+                input_file = args[i+1];
+            } else if (args[i].equalsIgnoreCase("-o")) {
+                output_file = args[i+1];
+            } else if (args[i].equalsIgnoreCase("-h")) {
+                System.out.println("Program's flags:  -i inputFile -o outputFile");
+            }
+        }
+
+        if (!(new File(input_file)).exists()){
+            System.out.println("Wrong flags! Use -h to see commands");
+            return;
+        }
 
         Map<String, Integer> occurrence_map = new HashMap<String, Integer>();
         countOccurrences(occurrence_map);
+        printOccurrenceMap(occurrence_map);
+        System.out.println("---------------------------------------");
+        System.out.println("Occurrence is finished!");
+
     }
 
-    private static void countOccurrences(Map<String,Integer> map){
+    public static void countOccurrences(Map<String,Integer> map) {
 
-        //read file into stream, try-with-resources
-        try (Stream<String> stream = Files.lines(Paths.get(input_file))) {
-
-            stream.forEach(System.out::println);
-
-        } catch (IOException e) {
+        // Create Hash (Occurrence Map)
+        try {
+            Files.lines(Paths.get(input_file))
+            .forEach(line -> {
+                // Split Line
+                String[] parts = line.split(" ");
+                for (int i=0; i <parts.length; i++) {
+                    if (!isNumeric(parts[i])){
+                        if (map.containsKey(parts[i])) {
+                            //key exists
+                            map.put(parts[i], map.get(parts[i]) + 1);
+                        } else {
+                            //key does not exists
+                            map.put(parts[i], 1);
+                        }
+                    }
+                }
+            });
+        }
+        catch (IOException e) {
             e.printStackTrace();
         }
+    }
 
+    public static void printOccurrenceMap(Map<String,Integer> map){
+        Iterator iterator = map.keySet().iterator();
+        System.out.println("Printing Occurrence Map!");
+        while (iterator.hasNext()) {
+            String key = iterator.next().toString();
+            Integer value = map.get(key);
+            System.out.println("Key: " + key + " - value: " + value);
+        }
+    }
 
+    public static boolean isNumeric(String str){
+        return str.matches("-?\\d+(\\.\\d+)?");
     }
 }

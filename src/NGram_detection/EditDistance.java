3d00e18834e6af19a54ee16d328724791d4b5895
Dimitris Mouris <jimmouris@hotmail.com>
2016-11-20 18:01:15 +0200

    Add EditDistance class from http://www.geeksforgeeks.org/dynamic-programming-set-5-edit-distance/

---
+++ src/NGram_detection/EditDistance.java
@@ -0,0 +1,38 @@
+package NGram_detection;
+
+/**
+ * Created by jimouris on 11/20/16.
+ */
+public class EditDistance {
+
+    private static int min(int x, int y, int z) {
+        if (x < y && x <z) return x;
+        if (y < x && y < z) return y;
+        else return z;
+    }
+
+    public static int editDist(String str1, String str2, int m, int n) {
+        // Create a table to store results of subproblems
+        int dp[][] = new int[m+1][n+1];
+
+        // Fill d[][] in bottom up manner
+        for (int i=0; i<=m; i++) {
+            for (int j=0; j<=n; j++) {
+                // If first string is empty, only option is to isnert all characters of second string
+                if (i==0)
+                    dp[i][j] = j;  // Min. operations = j
+                    // If second string is empty, only option is to remove all characters of second string
+                else if (j==0)
+                    dp[i][j] = i; // Min. operations = i
+                    // If last characters are same, ignore last char and recur for remaining string
+                else if (str1.charAt(i-1) == str2.charAt(j-1))
+                    dp[i][j] = dp[i-1][j-1];
+                    // If last character are different, consider all possibilities and find minimum
+                else
+                    dp[i][j] = 1 + min(dp[i][j-1], dp[i-1][j], dp[i-1][j-1]); // Insert, Remove, Replace
+            }
+        }
+        return dp[m][n];
+    }
+
+}

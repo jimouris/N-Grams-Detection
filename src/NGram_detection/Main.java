f89e3ab4fc295c7ed3e83b6283a5c1f7d864ef18
Nikolas Bompetsis <nbompetsis@di.uoa.gr>
2016-11-20 17:42:52 +0200

    Initial commmit

---
+++ src/NGram_detection/Main.java
@@ -0,0 +1,12 @@
+package NGram_detection;
+
+/**
+ * Created by nikolas on 20/11/16.
+ */
+public class Main {
+
+
+    public static void main(String[] args) {
+        System.out.println("ASDADADA");
+    }
+}

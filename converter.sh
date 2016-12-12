7477a0bf993b6f94066959213e2ac6938f43dbbb
Nikolas Bompetsis <nikolas.mpompetsis@gmail.com>
2016-12-12 16:02:33 +0200

    Minor fixies at converter.sh file

--- converter.sh
+++ converter.sh
@@ -1,14 +1,16 @@
 #!/bin/bash
 
+ftext=ftext.dat
+tmp=tmp.dat
 for var in "$@"
 do
 	if [ -e "$var" ]
   then
-  	paste -s -d ' ' $var > "test_$var"
-    rm $var
-    mv "test_$var" $var
+		paste -s -d ' ' $var > $tmp
+    sed -e "s/[[:space:]]\+/ /g"  $tmp >> $ftext
     echo "Your file ($var) has been converted successfully!"
   else
-  	echo "$var file doesn't exist."
+    echo "$var file doesn't exist."
   fi
 done
+rm $tmp
